195009cdc536e5771c6cd56c145ab54a70cdb03f
mateuszzieba <mz@cvt.services>
2023-10-26 06:21:18 +0200

    revert

--- ecommerce_api/models/order.py
+++ ecommerce_api/models/order.py
@@ -11,8 +11,8 @@ class Order(models.Model):
     client = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     delivery_address = models.CharField(max_length=300)
     products = models.ManyToManyField('ecommerce_api.Product', through='ecommerce_api.OrderProduct')
-    date_ordered = models.DateField(default=timezone.now)
-    payment_due = models.DateField(blank=True, null=True)
+    date_ordered = models.DateTimeField(default=timezone.now)
+    payment_due = models.DateTimeField(blank=True, null=True)
     total_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 
